6f5b3c9ec1bcae20922d786821748b7c006dbff9
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-16 21:25:53 +0300

    test code for preview generation

--- experimental_code/step04_BTBDB_DataEntry/run04_tmp_code_preview.py
+++ experimental_code/step04_BTBDB_DataEntry/run04_tmp_code_preview.py
@@ -2,5 +2,23 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ar'
 
+import os
+import numpy as np
+import nibabel as nib
+import app.core.preprocessing as preproc
+
 if __name__ == '__main__':
-    pass
+    fnii = '../../experimental_data/dataentry_test0/case-2c396a3e-1900-4fb4-bd3a-6763dc3f2ec0/study-dd10657e-f2c3-48ba-87d6-b5f3fc40c752/series-1.3.6.1.4.1.25403.163683357445804.6452.20140120113751.2-CT.nii.gz'
+    fniiMsk = '%s-lungs.nii.gz' % fnii
+    fniiLes = '%s-lesion.nii.gz' % fnii
+    sizPrv = 256
+    shpPrv = (sizPrv, sizPrv, sizPrv)
+    dataImg = preproc.niiImagePreTransform(nib.load(fnii).get_data())
+    dataMsk = preproc.niiImagePreTransform(nib.load(fniiMsk).get_data())
+    dataLes = preproc.niiImagePreTransform(nib.load(fniiLes).get_data())
+    #
+    dataImgR = preproc.resize3D(dataImg, shpPrv)
+    dataMskR = preproc.resize3D(dataMsk, shpPrv)
+    dataLesR = preproc.resize3D(dataLes, shpPrv)
+
+    print('--')
