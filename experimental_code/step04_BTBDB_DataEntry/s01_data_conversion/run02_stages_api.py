a836402b0d7367b5f08fc3ec9ce3d5e5f38957b9
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-23 09:18:40 +0300

    stage-api

---
+++ experimental_code/step04_BTBDB_DataEntry/s01_data_conversion/run02_stages_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'ar'
+
+
+"""
+status: 
+    - ok  : fiished
+    - in  : in progress
+    - err : 
+"""
+class StageInfo:
+
+    def __init__(self):
+        pass
+
+class SeriesProcStages:
+    def __init__(self, ):
+
+
+
+if __name__ == '__main__':
+    pass
