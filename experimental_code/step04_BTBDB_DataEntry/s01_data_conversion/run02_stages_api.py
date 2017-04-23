72e056310596cee8020f3dc384c858b48e12a980
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-23 11:16:48 +0300

    stub branch

--- experimental_code/step04_BTBDB_DataEntry/s01_data_conversion/run02_stages_api.py
+++ experimental_code/step04_BTBDB_DataEntry/s01_data_conversion/run02_stages_api.py
@@ -2,22 +2,39 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ar'
 
+import enum
+import json
 
 """
-status: 
-    - ok  : fiished
+status for stage/total: 
+    - new : all stages is incompleted
+    - ok  : is finished
     - in  : in progress
-    - err : 
+    - err : error, and
 """
-class StageInfo:
-
-    def __init__(self):
-        pass
+class StageStatus(enum.Enum):
+    STATE_NEW   = 'new'
+    STATE_OK    = 'ok'
+    STATE_IN    = 'in'
+    STATE_ERR   = 'err'
+    STATES = [STATE_NEW, STATE_OK, STATE_IN, STATE_ERR]
+    def __init__(self, pdict=None):
+        if pdict is None:
+            self._state = StageStatus.STATE_NEW
+            self._serr = None
+        else:
+            if 'state' in pdict:
+                pass
+            else:
+                self._state = StageStatus.STATE_ERR
+                self._serr = ''
 
 class SeriesProcStages:
-    def __init__(self, ):
-
-
+    FILE_STATUS = 'status.json'
+    def __init__(self, pdir=None):
+        pass
+    def buildNew(self):
+        ret = dict()
 
 if __name__ == '__main__':
     pass
