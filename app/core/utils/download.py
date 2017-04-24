cd106743ed6f4537c59bae726e94b592d6391fa3
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-24 21:39:13 +0300

    series download runner

--- app/core/utils/download.py
+++ app/core/utils/download.py
@@ -10,8 +10,10 @@ import json
 from pprint import pprint
 import errno
 
-import logging
-import logging.handlers
+from . import mkdir_p
+import log
+
+import mproc
 
 urlTakeList="https://data.tbportals.niaid.nih.gov/api/cases?since=2017-02-01&take=%d&skip=%d"
 urlCaseInfo="https://data.tbportals.niaid.nih.gov/api/cases/%s"
@@ -53,6 +55,41 @@ def downloadDicom(urlRequest, pauthTocken=None):
         print('*** ERROR: %s' % urlRequest)
         pprint(json.loads(tret._content))
         raise Exception(strErr)
+
+#######################################
+class ProcessRunnerDownloadSeries(mproc.ProcessRunner):
+    def __init__(self, series, isCleanBeforeStart = False):
+        self.series = series
+        self.is_clean = isCleanBeforeStart
+    def getUniqueKey(self):
+        return self.series.getKey()
+    def run(self):
+        if self.series.isInitialized():
+            wdir = self.series.getDir(isRelative=False)
+            wdirRaw = self.series.getDirRaw(isRelative=False)
+            if self.series.isHasData():
+                if self.is_clean:
+                    if os.path.isdir(wdir):
+                        shutil.rmtree(wdir)
+                mkdir_p(wdir)
+                mkdir_p(wdirRaw)
+                ptrLogger = log.get_logger(wdir=wdir, logName='stage1_donwload')
+                ptrLogger.info("start downloading : {0}".format(self.getUniqueKey()))
+                lstInstJs = self.series.getInstancesJs()
+                for instJs in lstInstJs:
+                    foutDicom = os.path.join(wdirRaw, self.series.getInstanceBaseNameJs(instJs))
+                    # FIXME: in future you need check downloaded file size to more accurate validation of "existing" DICOM
+                    if not os.path.isfile(foutDicom):
+                        ptrLogger.info('start downloading [{0}]'.format(foutDicom))
+                        try:
+                            urlDicom = instJs['content']['url']
+                            data = downloadDicom(urlDicom)
+                            with open(foutDicom, 'wb') as f:
+                                f.write(data.getvalue())
+                        except Exception as err:
+                            ptrLogger.error('Cant download DICOM URL for instance [{0}] : {1}'.format(instJs, err))
+                    else:
+                        ptrLogger.info('*** WARNING *** file [{0}] exist, skip'.format(foutDicom))
 
 #######################################
 if __name__ == '__main__':
