2197f157969c9b525fae1d94d702c7c4098fba77
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-25 00:21:50 +0300

    fix multiprocessing test

--- app/core/utils/mproc_test.py
+++ app/core/utils/mproc_test.py
@@ -1,10 +1,12 @@
+##
+
 import unittest
 
 import os
 import time
 from datetime import datetime
 import numpy as np
-from mproc import SimpleProcessManager, ProcessRunner
+from mproc import SimpleTaskManager, ProcessRunner
 
 ######################################
 class MyTask(ProcessRunner):
@@ -16,20 +18,33 @@ class MyTask(ProcessRunner):
         else:
             self.dt = 100
     def getUniqueKey(self):
-        return 'runner-{0}-{1}'.format(os.getpid(), datetime.now().strftime('%Y.%m.%d-%H.%M.%S:%f'))
+        return 'runner: timeId={0}'.format(datetime.now().strftime('%Y.%m.%d-%H.%M.%S:%f'))
     def run(self):
         time.sleep(self.dt)
-        print ('%s: p1=%s, p2=%s, dt=%s' % (os.getpid(), self.param1, self.param2, self.dt))
+        print ('pid = %s: p1=%s, p2=%s, dt=%s' % (os.getpid(), self.param1, self.param2, self.dt))
 
 ######################################
 class TestMProc(unittest.TestCase):
-    def test_simple_taskrunner(self):
-        tm = SimpleProcessManager(nproc=3)
+    def test_simple_taskrunner_as_process(self):
+        tm = SimpleTaskManager(nproc=3, isThreadManager=False)
         numTasks = 12
         rndSleep = np.random.randint(1, 3, numTasks)
         for ii in rndSleep:
             newRunner = MyTask(param1=np.random.rand(), param2=np.random.rand(), dt=ii)
-            print (':: [{0}] : {1}'.format(ii, newRunner.getUniqueKey()))
+            print ('Processed :: [{0}] : {1}'.format(ii, newRunner.getUniqueKey()))
+            tm.appendTaskRunner(newRunner)
+        tm.waitAll()
+        numTot, numReady, numSuccess = tm.getStat()
+        self.assertEqual(numTot,     numTasks)
+        self.assertEqual(numReady,   numTasks)
+        self.assertEqual(numSuccess, numTasks)
+    def test_simple_taskrunner_as_thread(self):
+        tm = SimpleTaskManager(nproc=3, isThreadManager=True)
+        numTasks = 12
+        rndSleep = np.random.randint(1, 3, numTasks)
+        for ii in rndSleep:
+            newRunner = MyTask(param1=np.random.rand(), param2=np.random.rand(), dt=ii)
+            print ('Threaded :: [{0}] : {1}'.format(ii, newRunner.getUniqueKey()))
             tm.appendTaskRunner(newRunner)
         tm.waitAll()
         numTot, numReady, numSuccess = tm.getStat()
