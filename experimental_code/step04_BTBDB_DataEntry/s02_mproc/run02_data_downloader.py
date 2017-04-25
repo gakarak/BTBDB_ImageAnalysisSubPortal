7245fc31f494c6bea7ffa53077758898593548a7
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-25 09:32:25 +0300

    fix test sample

--- experimental_code/step04_BTBDB_DataEntry/s02_mproc/run02_data_downloader.py
+++ experimental_code/step04_BTBDB_DataEntry/s02_mproc/run02_data_downloader.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 __author__ = 'ar'
 
+from app.core.utils.mproc import SimpleTaskManager
 from app.core.utils.download import RunnerDataEntry
 
 if __name__ == '__main__':
     runnerDataEntry = RunnerDataEntry()
     runnerDataEntry.refreshCases()
     print (runnerDataEntry)
+
+    # runnerDataEntry.run()
+
+    tmDataEntry = SimpleTaskManager(nproc=1, isThreadManager=True)
+    tmDataEntry.appendTaskRunner(runnerDataEntry)
+    tmDataEntry.waitAll(dt=5)
