22f90032e3157a99c215b10c6ff4b1fbc802389b
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-25 21:16:00 +0300

    backend app config

--- app/backend/__init__.py
+++ app/backend/__init__.py
@@ -1,7 +1,15 @@
 import os
 
 from flask import Flask
+from config import DevelopmentConfig
 
 app_flask = Flask(__name__, static_folder='../frontend', template_folder='../frontend')
+
+config = DevelopmentConfig()
+app_flask.config.from_object('config.DevelopmentConfig')
+
+if app_flask.config['DEBUG']:
+    print('** DIR_BASE = {0}'.format(app_flask.config['DIR_BASE']))
+    print('** DIR_DATA = {0}'.format(app_flask.config['DIR_DATA']))
 
 from app.backend import api
