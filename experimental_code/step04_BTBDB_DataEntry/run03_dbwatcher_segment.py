68717776bf3e76b2c337efd3a5b2d00d5464d9c1
Alexander Kalinovsky (MacBook) <gakarak@gmail.com>
2017-04-25 14:24:07 +0300

    fix example code with segm-api

--- experimental_code/step04_BTBDB_DataEntry/run03_dbwatcher_segment.py
+++ experimental_code/step04_BTBDB_DataEntry/run03_dbwatcher_segment.py
@@ -7,38 +7,48 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 from app.core.preprocessing import resizeNii
 from app.core.segmct import segmentLesions3D, segmentLungs25D
-import common as comm
+from app.core.dataentry_v1 import DBWatcher
+from app.core.segmct import api_segmentLungAndLesion
+# import common as comm
 
 if __name__ == '__main__':
     pathModelLung = '../../experimental_data/models/fcnn_ct_lung_segm_2.5d_tf/'
     pathModelLesion ='../../experimental_data/models/fcnn_ct_lesion_segm_3d_tf/'
     #
-    shape4Lung = (256, 256, 64)
-    shape4Lesi = (128, 128, 64)
+    # shape4Lung = (256, 256, 64)
+    # shape4Lesi = (128, 128, 64)
     dataDir = '../../experimental_data/dataentry_test0'
-    dbWatcher = comm.DBWatcher()
+    dbWatcher = DBWatcher(pdir=dataDir)
     dbWatcher.load(dataDir, isDropEmpty=True, isDropBadSeries=True)
-    dbWatcher.printStat()
+    # dbWatcher.printStat()
+    print (dbWatcher.toString())
     for ii, ser in enumerate(dbWatcher.allSeries()):
-        dataNii = nib.load(ser.pathNii)
-        shapeOrig = dataNii.shape
-        niiResiz4Lung = resizeNii(dataNii, shape4Lung)
-        niiResiz4Lesi = resizeNii(dataNii, shape4Lesi)
-        #
-        lungMask = segmentLungs25D(niiResiz4Lung,
-                                   dirWithModel=pathModelLung,
-                                   pathOutNii=None,
-                                   outSize=shapeOrig,
-                                   # outSize=shape4Lung,
-                                   threshold=0.5)
-        lesionMask = segmentLesions3D(niiResiz4Lesi,
-                                      dirWithModel=pathModelLesion,
-                                      pathOutNii=None,
-                                      outSize=shapeOrig,
-                                      # outSize=shape4Lung,
-                                      threshold=None)
-        foutLungMsk = '%s-lungs.nii.gz' % ser.pathNii
-        foutLesionMsk = '%s-lesion.nii.gz' % ser.pathNii
-        nib.save(lungMask, foutLungMsk)
-        nib.save(lesionMask, foutLesionMsk)
-        print ('[%d] --> [%s]' % (ii, foutLesionMsk))
+        if ser.isConverted():
+            api_segmentLungAndLesion(dirModelLung=pathModelLung,
+                                     dirModelLesion=pathModelLesion,
+                                     series=ser,
+                                     ptrLogger=None)
+            pathSegmLesion = ser.pathPostprocLesions(isRelative=False)
+            print ('[{0}] : isOk={2}, {1}'.format(ii, ser, os.path.isfile(pathSegmLesion)))
+        # dataNii = nib.load(ser.pathNii)
+        # shapeOrig = dataNii.shape
+        # niiResiz4Lung = resizeNii(dataNii, shape4Lung)
+        # niiResiz4Lesi = resizeNii(dataNii, shape4Lesi)
+        # #
+        # lungMask = segmentLungs25D(niiResiz4Lung,
+        #                            dirWithModel=pathModelLung,
+        #                            pathOutNii=None,
+        #                            outSize=shapeOrig,
+        #                            # outSize=shape4Lung,
+        #                            threshold=0.5)
+        # lesionMask = segmentLesions3D(niiResiz4Lesi,
+        #                               dirWithModel=pathModelLesion,
+        #                               pathOutNii=None,
+        #                               outSize=shapeOrig,
+        #                               # outSize=shape4Lung,
+        #                               threshold=None)
+        # foutLungMsk = '%s-lungs.nii.gz' % ser.pathNii
+        # foutLesionMsk = '%s-lesion.nii.gz' % ser.pathNii
+        # nib.save(lungMask, foutLungMsk)
+        # nib.save(lesionMask, foutLesionMsk)
+        # print ('[%d] --> [%s]' % (ii, foutLesionMsk))
