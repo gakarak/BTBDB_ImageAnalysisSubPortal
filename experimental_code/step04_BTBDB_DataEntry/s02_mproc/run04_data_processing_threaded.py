c6efc778fcefb4b19a8ba5148bd1681ba48c987d
Alexander Kalinovsky (DeepLab2) <gakarak@gmail.com>
2017-04-30 17:10:41 +0300

    very simple support multi-GPU multi-task DNN data processing (supprot only tensorflow backend)

---
+++ experimental_code/step04_BTBDB_DataEntry/s02_mproc/run04_data_processing_threaded.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'ar'
+
+from app.core.dataentry_v1 import DBWatcher
+from app.core.utils.mproc import SimpleTaskManager
+from app.core.utils.report import RunnerMakeReport
+
+import os
+from app.core.dataentry_v1 import DBWatcher
+from app.core.segmct import api_segmentLungAndLesion
+
+
+if __name__ == '__main__':
+    dataDir = 'data-cases'
+    pathModelLung = '../../../experimental_data/models/fcnn_ct_lung_segm_2.5d_tf/'
+    pathModelLesion = '../../../experimental_data/models/fcnn_ct_lesion_segm_3d_tf/'
+
+    # dbWatcher = DBWatcher(pdir=dataDir)
+    # for iser, ser in enumerate(dbWatcher.allSeries()):
+    #     if ser.isConverted() and (not ser.isPostprocessed()):
+    #         pathNii = ser.pathConvertedNifti(isRelative=False)
+    #         print (os.path.dirname(pathNii))
+    #         os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    #         api_segmentLungAndLesion(dirModelLung=pathModelLung,
+    #                                  dirModelLesion=pathModelLesion,
+    #                                  series=ser,
+    #                                  gpuMemUsage=0.2)
+    #         print ('---')
+
+    runnerMakeReport = RunnerMakeReport(data_dir=dataDir,
+                                        dirModelLung=pathModelLung,
+                                        dirModelLesion=pathModelLesion,
+                                        listGpuId=[0, 1])
+    #FIXME: this runner can be runned acynchronously over mproc.SimpleTaskManager
+    tm = SimpleTaskManager(nproc=4, isThreadManager=False)
+    runnerMakeReport.run(tm=tm)
+    tm.waitAll()
+    print ('---')
