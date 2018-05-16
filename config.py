0f2cf26f6177c9944891a02382271a036b24f3c9
Alexander Kalinovsky (armsi) <gakarak@gmail.com>
2018-05-16 12:38:22 +0300

    temporary config fix

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # DATA_DIR_NAME = 'data-real'
 # DATA_DIR_NAME = 'experimental_data/dataentry_test0'
-DATA_DIR_NAME = '/home/ar/data/crdf/test_dataentry0'
+# DATA_DIR_NAME = '/home/ar/data/crdf/test_dataentry0'
+DATA_DIR_NAME = '/mnt/s3-imlab.tbportal.org/test_dataentry0'
 
 class Config(object):
     DEBUG = True
